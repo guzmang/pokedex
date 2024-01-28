27a94dc3ec9969675f660528929fee509a70d69b
German Guzman <guzman.german1@gmail.com>
2024-01-28 03:55:53 -0300

    Se devuelve el pokemon

--- src/pokemons/pages/PokemonSearchByName.jsx
+++ src/pokemons/pages/PokemonSearchByName.jsx
@@ -1,70 +1,93 @@
+import { useState, useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import queryString from 'query-string'
+import axios from 'axios';
 
 import { useForm } from '../../hooks/useForm';
 import { PokemonCard } from '../components';
 
 export const PokemonSearchByName = () => {
 
-  const navigate = useNavigate();
-  const location = useLocation();
+    const navigate = useNavigate();
+    const location = useLocation();
+  
+    const { q = '' } = queryString.parse( location.search );
+  
+    const showSearch = (q.length === 0);
+    
+    const { searchText, onInputChange } = useForm({
+      searchText: q
+    }); 
 
-  const { q = '' } = queryString.parse( location.search );
+    const [pokemon, setPokemon] = useState(null);
+    const [loading, setLoading] = useState(false);
 
-  const showSearch = (q.length === 0);
+    useEffect(() => {
+        const { q } = queryString.parse(location.search);
+        if (q) {
+            const getPokemon = async() => {
+                try {
+                const { data } = await axios.get(`http://localhost:3000/api/name/${q}`);
+                setPokemon(data);
+                setLoading(false);
+                } catch (error) {
+                console.error('Error getting pokemon:', error);
+                setLoading(false);
+                }
+            };
+            getPokemon();
+        }
+    }, [location.search, loading]);
 
-  const { searchText, onInputChange } = useForm({
-    searchText: q
-  });
+    const onSearchSubmit = (event) =>{
+        event.preventDefault();
+        setLoading(true);
+        navigate(`?q=${ searchText }`);
+    }
 
-  const onSearchSubmit = (event) =>{
-    event.preventDefault();
-    // if ( searchText.trim().length <= 1 ) return;
+    if (loading) {
+        return <div>Loading...</div>;
+    }
 
-    navigate(`?q=${ searchText }`);
-  }
+    return (
+        <>
+        <h1>Search</h1> 
+        <hr />
 
+        <div className="row">
 
-  return (
-    <>
-      <h1>Search</h1> 
-      <hr />
+            <div className="col-5">
+                <h4>Searching</h4>
+                <hr />
+                <form onSubmit={ onSearchSubmit }>
+                <input 
+                    type="text"
+                    placeholder="Search a Pokemon"
+                    className="form-control"
+                    name="searchText"
+                    autoComplete="off"
+                    value={ searchText }
+                    onChange={ onInputChange }
+                />
 
-      <div className="row">
-
-          <div className="col-5">
-            <h4>Searching</h4>
-            <hr />
-            <form onSubmit={ onSearchSubmit }>
-              <input 
-                type="text"
-                placeholder="Search a Pokemon"
-                className="form-control"
-                name="searchText"
-                autoComplete="off"
-                value={ searchText }
-                onChange={ onInputChange }
-              />
-
-              <button className="btn btn-outline-primary mt-1">
-                Search
-              </button>
-            </form>
-          </div>
-
-          <div className="col-7">
-            <h4>Results</h4>
-            <hr />
-            
-            <div className="alert alert-primary animate__animated animate__fadeIn" 
-                style={{ display: showSearch ? '' : 'none' }}>
-              Search a Pokemon
+                <button className="btn btn-outline-primary mt-1">
+                    Search
+                </button>
+                </form>
             </div>
 
-          </div>
-      </div>
-      
+            <div className="col-7">
+                <h4>Results</h4>
+                <hr />
+                
+                <div className="alert alert-primary animate__animated animate__fadeIn" 
+                    style={{ display: showSearch ? '' : 'none' }}>
+                Search a Pokemon
+                </div>
 
-    </>
-  )
+            </div>
+        </div>
+    
+        </>
+    )
 }
