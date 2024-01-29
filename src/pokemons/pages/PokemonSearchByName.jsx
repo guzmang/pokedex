67877fab3110cf450e20c6921791ad926107ca04
German Guzman <guzman.german1@gmail.com>
2024-01-29 03:12:03 -0300

    Se agrega una validacion, se podria mejorar para mostrar un cartel de no encontrado o un useMemo para no llamar de nuevo si el pokemon es el mismo

--- src/pokemons/pages/PokemonSearchByName.jsx
+++ src/pokemons/pages/PokemonSearchByName.jsx
@@ -23,16 +23,18 @@ export const PokemonSearchByName = () => {
     const [loading, setLoading] = useState(false);
 
     useEffect(() => {
+        setPokemon(null);
         const { q } = queryString.parse(location.search);
         if (q) {
             const getPokemon = async() => {
                 try {
-                const { data } = await pokemonApi.get(`/name/${q}`);
-                setPokemon(data);
-                setLoading(false);
+                    const { data } = await pokemonApi.get(`/name/${q}`);
+                    setPokemon(data);
+                    setLoading(false);
                 } catch (error) {
-                console.error('Error getting pokemon:', error);
-                setLoading(false);
+                    console.error('Error getting pokemon:', error);
+                    setLoading(false);
+                    setPokemon(null);
                 }
             };
             getPokemon();
@@ -41,63 +43,64 @@ export const PokemonSearchByName = () => {
 
     const onSearchSubmit = (event) =>{
         event.preventDefault();
+        if ( searchText.trim().length < 1 ) return;
         setLoading(true);
         navigate(`?q=${ searchText }`);
     }
 
     if (loading) {
-        return <div>Loading...</div>;
+        return <h1>Loading...</h1>;
     }
 
     return (
         <>
-        <h1>Search</h1> 
-        <hr />
+            <h1>Search</h1> 
+            <hr />
 
-        <div className="row">
+            <div className="row">
 
-            <div className="col-5">
-                <h4>Searching</h4>
-                <hr />
-                <form onSubmit={ onSearchSubmit }>
-                <input 
-                    type="text"
-                    placeholder="Search a Pokemon"
-                    className="form-control"
-                    name="searchText"
-                    autoComplete="off"
-                    value={ searchText }
-                    onChange={ onInputChange }
-                />
+                <div className="col-5">
+                    <h4>Searching</h4>
+                    <hr />
+                    <form onSubmit={ onSearchSubmit }>
+                    <input 
+                        type="text"
+                        placeholder="Search a Pokemon"
+                        className="form-control"
+                        name="searchText"
+                        autoComplete="off"
+                        value={ searchText }
+                        onChange={ onInputChange }
+                    />
 
-                <button className="btn btn-outline-primary mt-1">
-                    Search
-                </button>
-                </form>
-            </div>
-
-            <div className="col-7">
-                <h4>Result</h4>
-                <hr />
-                
-                <div className="alert alert-primary animate__animated animate__fadeIn" 
-                    style={{ display: showSearch ? '' : 'none' }}>
-                    Search a Pokemon
+                    <button className="btn btn-outline-primary mt-1">
+                        Search
+                    </button>
+                    </form>
                 </div>
 
-                {
-                    pokemon && <PokemonCard 
-                                    key = { pokemon.id }
-                                    id = { pokemon.id }
-                                    name = { pokemon.name }
-                                    sprites = { pokemon.sprites }
-                                    stats = { pokemon.stats }
-                                    types = { pokemon.types }
-                                />
-                }
+                <div className="col-7">
+                    <h4>Result</h4>
+                    <hr />
+                    
+                    <div className="alert alert-primary animate__animated animate__fadeIn" 
+                        style={{ display: showSearch ? '' : 'none' }}>
+                        Search a Pokemon
+                    </div>
 
+                    {
+                        pokemon && <PokemonCard 
+                                        key = { pokemon.id }
+                                        id = { pokemon.id }
+                                        name = { pokemon.name }
+                                        sprites = { pokemon.sprites }
+                                        stats = { pokemon.stats }
+                                        types = { pokemon.types }
+                                    />
+                    }
+
+                </div>
             </div>
-        </div>
     
         </>
     )
